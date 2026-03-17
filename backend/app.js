b78b2d1097d6e49c8183611b99a92ce70b445e5d
amolpatil1955 <163282417+amolpatil1955@users.noreply.github.com>
2026-03-18 00:20:03 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5000"],
+    origin: ["https://nexusai-1-t7ea.onrender.com", "http://localhost:5000"],
     credentials: true,
   }),
 );
